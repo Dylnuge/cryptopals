46921f12ac8dbb5bd4621a41fd79825b71658f00
Dylan Nugent <dylnuge@gmail.com>
2017-12-22 12:48:52 -0600

    Finish breaking out scoring algorithm

--- cryptolib/score.go
+++ cryptolib/score.go
@@ -32,6 +32,43 @@ var EnglishAlphabetFrequencies map[byte]float64 = map[byte]float64{
     'z': 0.00074,
 }
 
+// A set of ASCII codepoints that should trigger rejection of a string
+var ASCIIAbnormalControlCharacters map[byte]bool = map[byte]bool{
+    // 0 is NUL, which may be a string terminator
+    1: true, // SOH
+    2: true, // STX
+    3: true, // ETX
+    4: true, // EOT
+    5: true, // ENQ
+    6: true, // ACK
+    7: true, // BEL
+    8: true, // BS (shouldn't really be in text)
+    // 9 is TAB, which may appear in valid text
+    // 10 is LF, which is a line terminator
+    11: true, // VT
+    12: true, // FF
+    // 13 is CR, which is part of CRLF windows-style terminated lines
+    14: true, // SO
+    15: true, // SI
+    16: true, // DLE
+    17: true, // DC1
+    18: true, // DC2
+    19: true, // DC3
+    20: true, // DC4
+    21: true, // NAK
+    22: true, // SYN
+    23: true, // ETB
+    24: true, // CAN
+    25: true, // EM
+    26: true, // SUB
+    27: true, // ESC
+    28: true, // FS
+    29: true, // GS
+    30: true, // RS
+    31: true, // US
+    127: true, // DEL (shouldn't really be in text)
+}
+
 
 /**
 Score a candidate text based only on each individual character.
@@ -43,18 +80,44 @@ characters that should never appear (like control characters).
 There are numerous flaws in this method; for instance, the string "EEEEEE" would
 score higher than "Friend", since "E" is a more frequent letter and therefore
 worth more points outright.
+
+Returns a float score which is 0 if no characters were found, -1 if a rejection
+character was found, and otherwise the sum of the frequencies of the letters.
+Higher scores indicate a more probable plaintext candidate.
 */
 func NaiveEnglishASCIIScore(text []byte) float64 {
     var score float64 = 0
 
     for i := 0; i < len(text); i++ {
         eval_char := text[i]
-        if eval_char >= 'A' && eval_char <= 'Z' {
-            // Ignore case; convert uppercase characters to lowercase
+        switch{
+        // Cases where character is a standard letter
+        case eval_char >= 'A' && eval_char <= 'Z':
             eval_char += 32
-        }
-        if eval_char >= 'a' && eval_char <= 'z' {
             score += EnglishAlphabetFrequencies[eval_char]
+        case eval_char >= 'a' && eval_char <= 'z':
+            score += EnglishAlphabetFrequencies[eval_char]
+        case eval_char == ' ':
+            // This is a cheap trick, and without it this function is pretty
+            // crappy at finding English strings. .16 is (1/6), which comes from
+            // the average word length in English being about 5 letters, meaning
+            // we'd expect about 1 in every 6 characters to be a space. But, of
+            // course, that requires that the plaintext even has spaces to
+            // begin with.
+            score += .16
+        case ASCIIAbnormalControlCharacters[eval_char]:
+            // Character is in the set of control characters we're assuming
+            // will never show up in a valid plaintext, so short-circut reject
+            // this candidate. There are problems with this (see the comment
+            // below), and our abnormal character set might be outright wrong.
+            return -1;
+        case eval_char > 127:
+            // Non ASCII codepoint. Short-circut reject this string outright.
+            // There are some problems with doing this; attack code needs to be
+            // nimble, so this might change later. It's easy to break this
+            // exploit by dropping a "corrupted" character into an otherwise
+            // valid plaintext, for instance.
+            return -1;
         }
     }
 
--- main.go
+++ main.go
@@ -7,60 +7,6 @@ import (
     "github.com/dylnuge/cryptopals/cryptolib"
 )
 
-func score_english_ascii(guess []byte) float64 {
-    // Starting with the extremely naive and effectively disqualifying things
-    // that use control characters. Numbers are neutral. Alpha characters add.
-    var score float64 = 0;
-
-    for i := 0; i < len(guess); i++ {
-        eval_char := guess[i];
-        switch {
-        case eval_char > 0 && eval_char < 9:
-            // Rare control characters for plaintext. Note that NUL is excluded,
-            // though in a realistic plaintext we'd only expect _one_ NUL (if
-            // any), and it'd be at the end of the plaintext.
-            score -= 100;
-        case eval_char > 10 && eval_char < 13:
-            // More rare control characters. Explicitly excludes TAB, LF, and
-            // CR.
-            score -= 100;
-        case eval_char > 13 && eval_char < 32:
-            // And the rest of the control characters. The separator characters
-            // might deserve consideration, but I don't see them a lot.
-            score -= 100;
-        case eval_char > 127:
-            // Non ASCII codepoints
-            score -= 100;
-        case eval_char == 0 || eval_char == 10 || eval_char == 13:
-            // These might appear, but a lot of them should reduce likelihood.
-            score -= 5;
-        case eval_char >= 35 && eval_char <= 47:
-            // These might appear, but a lot of them should reduce likelihood.
-            score -= 30;
-        case eval_char >= 58 && eval_char <= 62:
-            // These might appear, but a lot of them should reduce likelihood.
-            score -= 30;
-        case eval_char >= 91 && eval_char <= 96:
-            // These might appear, but a lot of them should reduce likelihood.
-            score -= 30;
-        case eval_char >= 123 && eval_char <= 127:
-            // These might appear, but a lot of them should reduce likelihood.
-            score -= 30;
-        case eval_char >= '0' && eval_char <= '9':
-            score -= 10;
-        // Common letters
-        case eval_char == ' ':
-            score += 1;
-        default:
-            eval_arr := make([]byte, 1)
-            eval_arr[0] = eval_char
-            score += cryptolib.NaiveEnglishASCIIScore(eval_arr)
-        }
-    }
-
-    return score;
-}
-
 /* Testing functions for problem set 1
 
 These should probably be pulled out into a unit test file. These are the test
@@ -79,7 +25,7 @@ func decode_single_byte_xor(encoded []byte) ([]byte, float64, byte) {
         var can_msg []byte = make([]byte, len(encoded));
         can_key[0] = byte(i);
         can_msg = cryptolib.DecryptXor(encoded, can_key);
-        score := score_english_ascii(can_msg);
+        score := cryptolib.NaiveEnglishASCIIScore(can_msg);
 
         if score > best_score {
             best_score = score;
