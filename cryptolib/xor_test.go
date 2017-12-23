d599a0c9e45a1e4c36a4e5e5fc1fe855bf9e1860
Dylan Nugent <dylnuge@gmail.com>
2017-12-22 22:36:59 -0600

    Fix case of test function name

--- cryptolib/xor_test.go
+++ cryptolib/xor_test.go
@@ -39,7 +39,7 @@ func TestDecryptXorSingleCharKey(t *testing.T) {
     }
 }
 
-func TestRepeatingKeyXOR(t *testing.T) {
+func TestRepeatingKeyXor(t *testing.T) {
     var testin string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
     var expect string = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
