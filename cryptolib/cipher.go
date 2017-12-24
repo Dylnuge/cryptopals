fc4edcaf64436c76ff16d2036ead6576393969df
Dylan Nugent <dylnuge@gmail.com>
2017-12-23 22:30:04 -0600

    Implement CBC mode AES (challenge 10)

--- cryptolib/cipher.go
+++ cryptolib/cipher.go
@@ -26,9 +26,9 @@ func processBlockCipherEcb(text []byte, cipherFunc func(dst, src []byte)) []byte
 /** PUBLIC FUNCTIONS **/
 
 /**
-Decrypt text using AES in ECB mode.
+Encrypt text using AES in ECB mode.
 
-This function assumes properly padded inputs and will not handle padding.
+Text will be padded with PKCS#7 padding before being encrypted
 
 ECB (Electronic Codebook) is an INSECURE method of encrypting a block cipher.
 Because it just repeats the same key without modification, it's easy to detect,
@@ -36,7 +36,37 @@ and it leaks information about the encrypted data. If you're using this method,
 it should be for some security research or similar project (like cryptopals!).
 There is a reason the go dev team decided not to include ECB mode in the libs.
 
-Again, IF YOU ARE USING THIS TO ENCRYPT REAL THINGS THOSE THINGS ARE BROKEN.
+IF YOU ARE USING THIS TO ENCRYPT REAL THINGS THOSE THINGS ARE BROKEN.
+
+Inputs:
+    plaintext []byte: The text to encrypt.
+    key []byte: The key for AES. Must be 16, 24, or 32 bytes
+
+Outputs:
+    []byte: The ciphertext produced by encrypting through AES-ECB
+    error: Error if key or text has an invalid size
+*/
+func EncryptAesEcb(plaintext []byte, key []byte) ([]byte, error) {
+    // Create an AES cipher block using the key. AES library will handle size
+    // check here and return an error if the key is not a valid length
+    cipherBlock, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    // Pad the plaintext
+    paddedPlaintext := PKCS7PadMessage(plaintext, uint(len(key)))
+
+    out := processBlockCipherEcb(paddedPlaintext, cipherBlock.Encrypt)
+    return out, nil
+}
+
+/**
+Decrypt text using AES in ECB mode.
+
+This function assumes properly padded inputs and will not handle padding.
+
+Again, IF YOU ARE USING THIS TO HANDLE REAL THINGS THOSE THINGS ARE BROKEN.
 
 Inputs:
     ciphertext []byte: The text to decrypt. Must be a multiple of 16 bytes.
@@ -64,21 +94,33 @@ func DecryptAesEcb(ciphertext []byte, key []byte) ([]byte, error) {
 }
 
 /**
-Encrypt text using AES in ECB mode.
+Encrypt text using AES in CBC mode.
 
-Text will be padded with PKCS#7 padding before being encrypted
+Text will be padded with PKCS#7 padding before being encrypted.
 
-IF YOU ARE USING THIS TO ENCRYPT REAL THINGS THOSE THINGS ARE BROKEN.
+CBC (Cipher Block Chaining) is a block cipher mode where each block of plaintext
+is XORed against the preceding ciphertext block before being encrypted. The
+first block is XORed against an initialization vector.
 
 Inputs:
     plaintext []byte: The text to encrypt.
     key []byte: The key for AES. Must be 16, 24, or 32 bytes
+    iv []byte: The initialization vector for CBC mode. Must be 16 bytes.
 
 Outputs:
-    []byte: The ciphertext produced by encrypting through AES-ECB
+    []byte: The ciphertext produced by encrypting through AES-CBC
     error: Error if key or text has an invalid size
 */
-func EncryptAesEcb(plaintext []byte, key []byte) ([]byte, error) {
+func EncryptAesCbc(plaintext []byte, key []byte, iv []byte) ([]byte, error) {
+    // TODO(dylan): Might be possible to reduce code duplication here, but at
+    // some point decomposing these functions becomes more trouble than its
+    // worth, since each one is just slightly different.
+
+    // Check that the iv is the same as the blocksize
+    if len(iv) != aes.BlockSize {
+        return nil, errors.New("IV must be same length as cipher blocks")
+    }
+
     // Create an AES cipher block using the key. AES library will handle size
     // check here and return an error if the key is not a valid length
     cipherBlock, err := aes.NewCipher(key)
@@ -89,8 +131,68 @@ func EncryptAesEcb(plaintext []byte, key []byte) ([]byte, error) {
     // Pad the plaintext
     paddedPlaintext := PKCS7PadMessage(plaintext, uint(len(key)))
 
-    out := processBlockCipherEcb(paddedPlaintext, cipherBlock.Encrypt)
-    return out, nil
+    lastCipherBlock := iv
+    ciphertext := make([]byte, len(paddedPlaintext))
+    for block := 0; block < len(paddedPlaintext) / aes.BlockSize; block++ {
+        blockStart := block * aes.BlockSize;
+        blockEnd := (block + 1) * aes.BlockSize;
+
+        // XOR the plaintext with the last cipherblock
+        plainBlock := paddedPlaintext[blockStart:blockEnd]
+        xorPlainBlock := DecryptXor(plainBlock, lastCipherBlock)
+        cipherBlock.Encrypt(ciphertext[blockStart:blockEnd], xorPlainBlock)
+        lastCipherBlock = ciphertext[blockStart:blockEnd]
+    }
+
+    return ciphertext, nil
+}
+
+/**
+Decrypt text using AES in CBC mode.
+
+Inputs:
+    ciphertext []byte: The text to encrypt.
+    key []byte: The key for AES. Must be 16, 24, or 32 bytes
+    iv []byte: The initialization vector for CBC mode. Must be 16 bytes.
+
+Outputs:
+    []byte: The ciphertext produced by encrypting through AES-CBC
+    error: Error if key or text has an invalid size
+*/
+func DecryptAesCbc(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
+    // Check that the ciphertext is divisible into 16 byte blocks
+    if len(ciphertext) % aes.BlockSize != 0 {
+        return nil, errors.New("Text is not a multiple of cipher blocksize")
+    }
+    // And check that the iv is the same as the blocksize
+    if len(iv) != aes.BlockSize {
+        return nil, errors.New("IV must be same length as cipher blocks")
+    }
+
+    // Create an AES cipher block using the key. AES library will handle size
+    // check here and return an error if the key is not a valid length
+    cipherBlock, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    lastCipherBlock := iv
+    plaintext := make([]byte, len(ciphertext))
+    for block := 0; block < len(ciphertext) / aes.BlockSize; block++ {
+        blockStart := block * aes.BlockSize;
+        blockEnd := (block + 1) * aes.BlockSize;
+
+        // Make a temporary block for the pre-xored plaintext
+        plainBlock := make([]byte, aes.BlockSize)
+
+        cipherBlock.Decrypt(plainBlock, ciphertext[blockStart:blockEnd])
+        // XOR the plaintext with the last cipherblock
+        xorPlainBlock := DecryptXor(plainBlock, lastCipherBlock)
+        copy(plaintext[blockStart:blockEnd], xorPlainBlock)
+        lastCipherBlock = ciphertext[blockStart:blockEnd]
+    }
+
+    return plaintext, nil
 }
 
 /**
--- cryptolib/cipher_test.go
+++ cryptolib/cipher_test.go
@@ -8,7 +8,7 @@ import (
 
 // This is cryptopals challenge 7
 func TestDecryptAesEcb(t *testing.T) {
-    b64Input, err := ioutil.ReadFile("fixtures/aes_encrypted.txt")
+    b64Input, err := ioutil.ReadFile("fixtures/aes_ecb_encrypted.txt")
     if err != nil {
         t.Errorf("Test fixture read error %v\n", err)
     }
@@ -36,7 +36,7 @@ func TestEncryptAesEcb(t *testing.T) {
         t.Errorf("Test fixture read error %v\n", err)
     }
 
-    b64Output, err := ioutil.ReadFile("fixtures/aes_encrypted.txt")
+    b64Output, err := ioutil.ReadFile("fixtures/aes_ecb_encrypted.txt")
     if err != nil {
         t.Errorf("Test fixture read error %v\n", err)
     }
@@ -45,6 +45,55 @@ func TestEncryptAesEcb(t *testing.T) {
 
     // Step 2: Decrypt it using the key provided
     ciphertext, err := EncryptAesEcb(plaintext, key)
+    if err != nil {
+        t.Errorf("Error occured during decrypt: %v\n", err)
+    }
+
+    if !bytes.Equal(ciphertext, expectedCiphertext) {
+        t.Errorf("Ciphertext does not match fixture:\n%v\n", ciphertext)
+    }
+}
+
+func TestDecryptAesCbc(t *testing.T) {
+    b64Input, err := ioutil.ReadFile("fixtures/aes_cbc_encrypted.txt")
+    if err != nil {
+        t.Errorf("Test fixture read error %v\n", err)
+    }
+    expectedOut, err := ioutil.ReadFile("fixtures/aes_decrypted.txt")
+    if err != nil {
+        t.Errorf("Test fixture read error %v\n", err)
+    }
+    ciphertext := DecodeBase64(string(b64Input))
+    key := []byte("YELLOW SUBMARINE")
+
+    // Step 2: Decrypt it using the key provided
+    iv := make([]byte, 16)
+    plaintext, err := DecryptAesCbc(ciphertext, key, iv)
+    if err != nil {
+        t.Errorf("Error occured during decrypt: %v\n", err)
+    }
+
+    if !bytes.Equal(plaintext, expectedOut) {
+        t.Errorf("Decoded plaintext does not match fixture:\n%v\n", plaintext)
+    }
+}
+
+func TestEncryptAesCbc(t *testing.T) {
+    plaintext, err := ioutil.ReadFile("fixtures/aes_decrypted.txt")
+    if err != nil {
+        t.Errorf("Test fixture read error %v\n", err)
+    }
+
+    b64Output, err := ioutil.ReadFile("fixtures/aes_cbc_encrypted.txt")
+    if err != nil {
+        t.Errorf("Test fixture read error %v\n", err)
+    }
+    expectedCiphertext := DecodeBase64(string(b64Output))
+    key := []byte("YELLOW SUBMARINE")
+
+    // Step 2: Decrypt it using the key provided
+    iv := make([]byte, 16)
+    ciphertext, err := EncryptAesCbc(plaintext, key, iv)
     if err != nil {
         t.Errorf("Error occured during decrypt: %v\n", err)
     }
