bb13698bfbd0d811a67a91e2683c847f0333a73e
Dylan Nugent <dylnuge@gmail.com>
2017-12-26 14:08:50 -0600

    Messy implementation of challenge 12

--- cryptolib/detection.go
+++ cryptolib/detection.go
@@ -1,5 +1,7 @@
 package cryptolib
 
+import "errors"
+
 /**
 Detect if a given ciphertext was encrypted in ECB mode.
 
@@ -13,9 +15,6 @@ func DetectEcbMode(ciphertext []byte, blocksize int) bool {
         return false
     }
 
-    // TODO(dylan): I'm worried about this methodology. If a byte array contains
-    // a null character, does go include that in the string, or terminate the
-    // string there?
     blockFreq := map[string]int {}
     for blockNum := 0; blockNum < len(ciphertext) / blocksize; blockNum++ {
         blockStart := blockNum * blocksize;
@@ -31,4 +30,37 @@ func DetectEcbMode(ciphertext []byte, blocksize int) bool {
         }
     }
     return false
+}
+
+/**
+Determine the blocksize of a cipher function presumed to be using an ECB cipher
+*/
+func DetermineEcbBlocksize(cipherFunc func([]byte) []byte, maxGuess int) (int, error) {
+    // We start at 2 because a blocksize of 1 byte is so small that we're nearly
+    // guarenteed to find duplicates when running DetectEcbMode, and they're a
+    // false positive. As with a lot of this code, we're basically just
+    // producing a best guess; there's no guarentee that finding duplicates with
+    // a blocksize guess of 4 determines _for sure_ that the blocksize is 4. It
+    // should work to "double check" by using three, four, five blocks and
+    // confirming we get three, four, five duplicate outputs, but we'll ignore
+    // that for now
+    for blocksize := 2; blocksize <= maxGuess; blocksize++ {
+        // Construct a message that is two "blocks" long
+        message := make([]byte, blocksize*2)
+        for i := 0; i < blocksize*2; i++ {
+            message[i] = 'A'
+        }
+
+        // Encrypt the message using the cipher function. This does NOT assume
+        // that the cipher function only encrypts the message and no other data
+        ciphertext := cipherFunc(message)
+
+        // Detect ECB mode. If detection works, we've found the right blocksize
+        if DetectEcbMode(ciphertext, blocksize) {
+            return blocksize, nil
+        }
+    }
+    // We should break out of the loop if we found the right blocksize, so we
+    // didn't. Return an error
+    return 0, errors.New("Cipher not using ECB, not a block cipher, or blocks are longer than maximum guess")
 }
--- pset2/main.go
+++ pset2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "bytes"
     "fmt"
     "github.com/dylnuge/cryptopals/cryptolib"
     "math/rand"
@@ -49,6 +50,8 @@ func randEncrypt(data []byte) (out []byte) {
 }
 
 func challenge11() {
+    // Seed RNG with the time this program was started.
+    rand.Seed(time.Now().UnixNano())
     data := []byte("Some arbitrary daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaata that will be encrypted. The quick brown fox jumps over the craaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazy dog")
     ciphertext := randEncrypt(data)
     if cryptolib.DetectEcbMode(ciphertext, 16) {
@@ -58,11 +61,131 @@ func challenge11() {
     }
 }
 
+func secretEncrypter(userInput []byte) []byte {
+    secret := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+    decodedSecret := cryptolib.DecodeBase64(secret)
+    // Use consistent random key ("random" seed was chosen arbitrairly by me)
+    rand.Seed(1711986)
+    key := genRandomBytes(16)
+    message := append(userInput, decodedSecret...)
+    out, err := cryptolib.EncryptAesEcb(message, key)
+    if err != nil {
+        fmt.Printf("ERROR encoding message %v\n", err)
+    }
+    return out
+}
+
+// Build a map from ciphertexts to plaintext bytes
+func buildCipherMappings(knownBlock []byte, blocksize int) map[string]byte {
+    if len(knownBlock) != blocksize - 1 {
+        fmt.Println("Sanity check failed, buildCipherMappings must have all but one byte\n")
+        return nil
+    }
+
+    output := map[string]byte{}
+    // for every possible byte
+    for i := 0; i <= int(^byte(0)); i++ {
+        guessByte := byte(i)
+        input := append(knownBlock, guessByte)
+        ciphertext := secretEncrypter(input)
+        cipherblock := ciphertext[0:blocksize]
+        output[string(cipherblock)] = guessByte
+    }
+
+    return output
+}
+
+// This is Cryptopals Challenge 12: Write an exploit to decrypt ECB block
+// ciphers when you're able to feed in part (but not all) of the plaintext to
+// the beginning of the message
+func crackECBMode() {
+    // Step 1: discover block size of cipher being used by secret encrypter. If
+    // this succeeds, the cipher is using ECB. If it fails, the cipher may not
+    // be using ECB, or the cipher may be using a blocksize larger than 64 bytes
+    // (We know it's using a 16 byte block cipher, specifically, AES)
+    blocksize, err := cryptolib.DetermineEcbBlocksize(secretEncrypter, 64)
+    if err != nil {
+        fmt.Printf("%v\n", err)
+        return
+    }
+
+    fmt.Printf("ECB block cipher detected! Blocksize: %v bytes\n", blocksize)
+
+    // Step 2: Determine the number of blocks in the message without user
+    // content. This is probably (not certainly) padded somehow, which may
+    // produce some issues at the end of the message decryption. We'll get to
+    // those in a bit.
+    ciphertext := secretEncrypter([]byte(""))
+    if len(ciphertext) % blocksize != 0 {
+        fmt.Printf("Original blocksize guess was incorrect.\n")
+        return
+    }
+    unknownBlocks := len(ciphertext) / blocksize
+    fmt.Printf("Secret message block length: %v\n", unknownBlocks)
+
+    // Step 3: Build a pretext block we can use to offset the message so we're
+    // only dealing with one unknown byte in a plaintext at a time. Block
+    // ciphers should have identical length input and output blocks, meaning we
+    // can in ECB mode map each block in the pretext to an output block.
+    knownPretext := make([]byte, blocksize)
+    for i := 0; i < blocksize; i++ {
+        // Strictly this is unnecessary; make initializes to null ('\x00') bytes
+        // anyways, but this makes things we print out to the command line
+        // more readable.
+        knownPretext[i] = 'A'
+    }
+    knownSecret := make([]byte, 0, len(ciphertext))
+
+    // Step 4: For each unknown byte in the secret message, encrypt and build a
+    // dictionary of possible plaintext input and ciphertext output blocks.
+    // Let's assume nothing about the plaintext and therefore check all 256
+    // possible bytes, not just likely ASCII ones.
+    for i := 0; i < len(ciphertext); i++ {
+        // Calculate byte offset as blocksize minus the byte we're looking for
+        // modded by the blocksize. The byte we're looking for is one beyond the
+        // last one we have; e.g. if we're looking at index 0, we're looking for
+        // the first byte, at index 1, we're looking for the second byte.
+        // byteOffset will tell us how much to pad the plaintext and block will
+        // tell us where to look.
+        byteOffset := blocksize - ((i+1) % blocksize)
+        // TODO this math seems annoyingly complex. It should be cleaner to
+        // "count down" the number of bytes we need to offset by
+        if byteOffset == blocksize {
+            byteOffset = 0
+        }
+        pretext := knownPretext[0:byteOffset]
+        knownBlock := make([]byte, 0, blocksize - 1)
+        // Build the known block from the last available characters of the known
+        // secret, and the pretext if we're still in the first block
+        // TODO again this code feels very complex for what it's actually doing
+        if len(knownSecret) < blocksize - 1 {
+            // If we're still in the first block, append from the pretext
+            for j := i; j < blocksize - 1; j++ {
+                knownBlock = append(knownBlock, 'A')
+            }
+            knownBlock = append(knownBlock, knownSecret...)
+        } else {
+            knownBlock = knownSecret[len(knownSecret) - (blocksize - 1):]
+        }
+
+        outputs := buildCipherMappings(knownBlock, blocksize)
+
+        // Step 5: Encrypt the secret, grab the block we're looking at, and get
+        // the byte from the cipher mappings we just built
+        currentCiphertext := secretEncrypter(pretext)
+        block := i / blocksize
+        blockStart := blocksize * block
+        blockEnd := blocksize * (block + 1)
+        currentCipherblock := currentCiphertext[blockStart:blockEnd]
+        nextByte := outputs[string(currentCipherblock)]
+        knownSecret = append(knownSecret, nextByte)
+        fmt.Printf("Found byte %v, known secret is now %v\n", nextByte, string(knownSecret))
+    }
+}
+
 func main() {
-    // Seed RNG with the time this program was started.
-    rand.Seed(time.Now().UnixNano())
-    challenge11()
-    challenge11()
-    challenge11()
-    challenge11()
+    if !bytes.Equal(secretEncrypter([]byte("mytest")), secretEncrypter([]byte("mytest"))) {
+        fmt.Println("Sanity check failed, key is changing between encodings\n")
+    }
+    crackECBMode()
 }
